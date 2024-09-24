413c16ba70e570dbb05f96d911937c231bfa433d
takeshi-kishima <yahoo.fukuoka@gmail.com>
2024-09-24 10:37:36 +0900

    ページング状態にしておく

---
+++ layouts/default.vue
@@ -0,0 +1,21 @@
+<script setup></script>
+
+<template>
+  <div>
+    <header>header</header>
+    <slot /><!-- 子要素 -->
+    <footer>&copy; foot</footer>
+  </div>
+</template>
+
+<style scoped>
+header {
+  background: #00000042;
+  color: #fff;
+  text-align: center;
+}
+footer {
+  background: #00000042;
+  text-align: center;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+console.log("メイン画面");
+</script>
+
+<template>
+  <div>メイン画面</div>
+</template>
+
+<style scoped></style>
