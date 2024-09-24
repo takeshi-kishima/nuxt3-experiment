7849d11c745da1c32967c9fc743021be3b17f928
takeshi-kishima <yahoo.fukuoka@gmail.com>
2024-09-24 11:08:32 +0900

    動的ルーティング

--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,18 @@ console.log("メイン画面");
 </script>
 
 <template>
-  <div>メイン画面</div>
+  <div>
+    メイン画面
+    <!-- 5回ループして<NuxtLink to="/users/[]">タグを書く -->
+    <div
+      v-for="i in 5"
+      :key="i"
+    >
+      <NuxtLink :to="`/users/${i}`">
+        ユーザー{{ i }}
+      </NuxtLink>
+    </div>
+  </div>
 </template>
 
 <style scoped></style>
---
+++ pages/users/[id].vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+const route = useRoute();
+</script>
+
+<template>
+  <div>
+    <h1>users/{{ route.params.id }}</h1>
+  </div>
+</template>
+
+<style scoped></style>
